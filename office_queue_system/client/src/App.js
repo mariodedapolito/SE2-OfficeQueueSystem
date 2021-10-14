a0899902054c16f71171f0c55d37e24761562908
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-10-14 11:03:38 +0200

    The component and route for admin page was created

--- office_queue_system/client/src/App.js
+++ office_queue_system/client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Switch, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import{Container, Button} from 'react-bootstrap'
+import {React} from 'react'
+import AdminPage from './Components/AdminPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Container fluid > 
+    <Switch>
+    <Route path="/admin"> 
+    <AdminPage/>
+    </Route>
+    <Route path="/"> 
+    <h3>This is the main page</h3>
+    <Link variant="link" to="/admin" >Go to the admin page</Link>
+    </Route>
+   <Redirect to="/"/>
+    </Switch>
+  </Container>
+  </Router>
   );
 }
 
---
+++ office_queue_system/client/src/Components/AdminPage.js
@@ -0,0 +1,22 @@
+
+import "../App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function AdminPage(props) {
+    return (
+        <>
+        <div className="loginContainer">
+        <div className="loginForm" >
+       <h2>This is admin page</h2>
+        </div>
+        </div>
+        </>
+        )
+      
+
+};
+
+
+
+
+export default AdminPage;
