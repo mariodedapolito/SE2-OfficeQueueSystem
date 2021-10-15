4e4e0747d2a6690d02e2f11d6313690825a84fd9
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-10-15 09:56:34 +0200

    Add files via upload

--- office_queue_system/client/src/App.js
+++ office_queue_system/client/src/App.js
@@ -1,27 +1,95 @@
+// Stile CSS
 
 import './App.css';
-import { BrowserRouter as Router, Redirect, Route, Switch, Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import{Container, Button} from 'react-bootstrap'
-import {React} from 'react'
-import AdminPage from './Components/AdminPage'
+
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch,Link } from 'react-router-dom';
+import {  Button} from 'react-bootstrap';
+import {LoginForm,LogoutButton} from './LoginForm';
+import API from './API';
+import Modal from 'react-bootstrap/Modal'
+
+function BLogin(){
+ return(<>
+<Button style={{fontSize: 20}}variant={"light"} ><Link to="/login">Login</Link></Button>
+</>
+);}
+ 
+
+
 
 function App() {
-  return (
-    <Router>
-    <Container fluid > 
-    <Switch>
-    <Route path="/admin"> 
-    <AdminPage/>
-    </Route>
-    <Route path="/"> 
-    <h3>This is the main page</h3>
-    <Link variant="link" to="/admin" >Go to the admin page</Link>
-    </Route>
-   <Redirect to="/"/>
-    </Switch>
-  </Container>
+const [show, setShow] = useState(false);
+const [message,setMessage]=useState('');
+const [me,setMe]=useState('');
+const [admin,setAdmin]=useState('');
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
+
+
+/*USEFFECT LOGIN*/
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await API.getUserInfo();
+        
+      } catch (err) {
+        console.error(err.error);
+      }
+    };
+    checkAuth();
+  }, []);
+ const doLogIn = async (credentials) => {
+    try {
+      const user = await API.logIn(credentials);
+     
+     
+      handleShow();
+      setMessage({ msg: `Welcome, ${user.name}!`, type: 'success' });
+      setMe('');
+     
+       setAdmin(`${user.admin}`);
+    } catch (err) {
+     
+      setMe('Password and/or email are not correct');
+    }
+  }
+
+
+  const doLogOut = async () => {
+    await API.logOut();
+    
+   
+   
+    
+  }
+
+return (
+  <Router>
+
+ {/* Modale di benvenuto */}
+        <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body> {message.msg}</Modal.Body>
+        <Modal.Footer>
+        </Modal.Footer>
+      </Modal>
+        
+        <Switch>
+          
+            <Route path="/login" render={()=><LoginForm me={me}login={doLogIn} admin={admin} />}/>
+             <Route path="/admin" render={()=><LogoutButton logout={doLogOut}/>}/>
+               <Route path="/officer" render={()=><LogoutButton logout={doLogOut}/>}/>
+
+           <Route path="/" render={()=><BLogin/>}/>
+
+        </Switch>
+
   </Router>
+    
   );
 }
 
