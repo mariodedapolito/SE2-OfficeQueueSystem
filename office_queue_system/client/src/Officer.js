680ef7aabdb906cc5c012e89f3a341d44e2858b5
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-10-18 00:36:32 +0200

    Update Officer.js

--- office_queue_system/client/src/Officer.js
+++ office_queue_system/client/src/Officer.js
@@ -3,4 +3,16 @@ function officer(officer_id,desk_id, username){
     this.desk_id=desk_id;
     this.username=username;
     }
-export default officer;
+
+function service(service_id, service_name, service_time){
+    
+    this.service_id=service_id;
+    this.service_name=service_name;
+    this.service_time=service_time;
+    }
+function services_desks(desk_id, service_id){
+    this.desk_id=desk_id;
+    this.service_id=service_id;
+   
+    }
+export  {officer,service,services_desks};
