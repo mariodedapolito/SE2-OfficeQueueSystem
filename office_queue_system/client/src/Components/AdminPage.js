8a8cad12e6edc7a6bbca0cb101c27a743b1f040d
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-10-18 22:03:14 +0200

    Update AdminPage.js

--- office_queue_system/client/src/Components/AdminPage.js
+++ office_queue_system/client/src/Components/AdminPage.js
@@ -1,10 +1,18 @@
 
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import {LogoutButton} from '../LoginForm';
+import API from '../API';
+import {Row, Button} from 'react-bootstrap';
+import { useState} from 'react';
 function AdminPage(props) {
     return (
         <>
+
+<Row>
+   <LogoutButton logout={props.logout}/>
+
+<Button style={{ fontSize: 20 }} style={{'position':'absolute' , 'right':'10px'}}onClick={()=>{API.deleteAllTickets().then(props.setRecharged(true)); }}>RESET QUEUES</Button></Row>
         <div className="loginContainer">
         <div className="loginForm" >
        <h2>This is admin page</h2>
@@ -20,3 +28,4 @@ function AdminPage(props) {
 
 
 export default AdminPage;
+
