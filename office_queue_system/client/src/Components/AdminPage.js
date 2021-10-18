430454a526474a0d950f6200499086397dfd8f95
Atabay Heydarli <62261953+HeydarliAtabay@users.noreply.github.com>
2021-10-19 01:03:43 +0200

    Task#7: Admin page component was implemented
    Modal for services, and Services table were added

--- office_queue_system/client/src/Components/AdminPage.js
+++ office_queue_system/client/src/Components/AdminPage.js
@@ -17,6 +17,18 @@ function DeskItem(props){
       )
   }
 
+
+  function ServiceItem(props){
+    const {service} = props
+    return(
+      <>
+    <td>{service.service_id}</td>
+    <td>{service.service_name}</td>
+    <td>{service.service_time}</td>                 
+    </>
+    )
+}
+
   function ModalForDesks(props) {
     const { fulldesks, desks, onClose, onSave, onDelete, services } = props;
     const [selectedDesk, setSelectedDesk]=useState(1)
@@ -101,15 +113,90 @@ function DeskItem(props){
     );
   }  
 
+   function ModalForServices(props) {
+    const {onClose,services, onSave} = props;
+    const [service, setService]=useState(services[0].service_name)
+    const [waitingTime, setWaitingTime]=useState(services[0].service_time)
+    
+
+    const handleSubmit = (event) => {
+      // stop event default and propagation
+      event.preventDefault();
+      event.stopPropagation();
+  
+      onSave(service, waitingTime);
+      window.location.reload();
+    };
+
+    
+    return (
+      <div className="cont">
+        <Modal show onHide={onClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Configure Services</Modal.Title>
+          </Modal.Header>
+          <Form onSubmit={handleSubmit}>
+            <Modal.Body>
+              <Form.Group controlId="form-description">
+                <Form.Label>Select Service</Form.Label>
+                <Form.Control
+                  as="select"
+                  name="desk"
+                  value={service}
+                  onChange={(ev) => {
+                    setService(ev.target.value);
+                    let serviceId=ev.target.value
+                    for(let i=0;i<services.length;i++){
+                      if(services[i].service_name===serviceId){
+                        setWaitingTime(services[i].service_time)
+                      }
+                    }
+                  }}
+                >
+                  {[...services].map((q,index) => {
+                    return <option>{q.service_name}</option>;
+                  })}
+                </Form.Control>
+              </Form.Group>
+              <Form.Group controlId="form-description">
+                <Form.Label>Waiting Time</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="description"
+                  value={waitingTime}
+                  onChange={(ev) => setWaitingTime(parseInt(ev.target.value))}
+                  required
+                  autoFocus>
+                </Form.Control>
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button variant="success" type="submit">
+                Update Service
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      </div>
+    );
+  }  
+
 
 function AdminPage(props) {
-    const {desks,services, deskids}=props
+   const {desks,services, deskids}=props
 
     const MODAL = { CLOSED: -2, ADD: -1 };
     const [selectedTask, setSelectedTask] = useState(MODAL.CLOSED);
+    const [selectedS, setSelectedS] = useState(MODAL.CLOSED);
 
     const handleClose = () => {
         setSelectedTask(MODAL.CLOSED);
+      }
+      const handleCloseS = () => {
+        setSelectedS(MODAL.CLOSED);
       }
 
       const handleSaveDesks = (deskId, serviceId) => {
@@ -128,6 +215,16 @@ function AdminPage(props) {
 
       function deleteDeskService (deskId,serviceId)  {
         API.deleteServiceOfDesk(deskId, serviceId).then((err)=>{})
+      }
+
+      //// new////
+      const handleSaveServices = (serviceName, time) => {
+        updateService(serviceName, time)
+        setSelectedS(MODAL.CLOSED); 
+      } 
+
+      function updateService (serviceName,time)  {
+        API.updateServiceTime(serviceName, time).then((err)=>{})
       }
     return (
         <>
@@ -158,13 +255,16 @@ function AdminPage(props) {
         <Card >
             <Card.Body> <Card.Title>Configure Services</Card.Title>
                 <Card.Text>Here you can add configure(change waiting time) and remove Services</Card.Text>
-                <Button variant="primary">Start</Button>
+                <Button variant="primary" size="lg"  onClick={() => setSelectedS(MODAL.ADD)}>Start</Button>
+                {(selectedS !== MODAL.CLOSED) && <ModalForServices services={services}  onSave={handleSaveServices} onClose={handleCloseS}></ModalForServices>}
             </Card.Body>      
         </Card>
         </Col>
         </Row>
         <div className="DeskItem">
 
+           <Row>
+           <Col>
            <Container fluid>
            <Table striped bordered hover>
   <thead>
@@ -195,14 +295,37 @@ function AdminPage(props) {
   </tbody>
 </Table>
               </Container>
+              </Col>
+              <Col>
+              <Container fluid>
+           <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Service id</th>
+      <th>Service Name</th>
+      <th>Service Time</th>
+    </tr>
+  </thead>
+  <tbody>
+      {
+          services.map(d => {
+            return (
+                <>
+                <tr>
+                <ServiceItem service={d} />
+              </tr>
+              </>
+            );
+          })
+      }
+  </tbody>
+</Table>
+              </Container>
+              </Col>
+              </Row>
             </div>
         </div>  
     </Container>
-        
-      
-       
-
-
         </>
         )
       
