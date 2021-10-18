30e2d78c6134f8378887bb77ca222f44f8f728e9
fridanco <79710958+fridanco@users.noreply.github.com>
2021-10-18 15:43:48 +0200

    SelectService completed

--- office_queue_system/client/src/Components/SelectServices.js
+++ office_queue_system/client/src/Components/SelectServices.js
@@ -1,25 +1,41 @@
-import { Button, Col, Row, Container } from "react-bootstrap";
+import { Button, Col, Row, Container, Alert } from "react-bootstrap";
+import { useState } from "react";
+import { service } from "../Officer";
+import API from "../API";
+
 function SelectServices(props) {
-  let services=["Payments","Mail Delivery", "Investiments" ,"Customer Support"]
-  const rows = [...Array(Math.ceil(services.length / 2))];
+  const [ticketIdgenerated, setTicketIdGenerated] = useState(-1);
+  const [show, setShow] = useState(false);
+  const rows = [...Array(Math.ceil(props.servicesList.length / 2))];
   const serviceRows = rows.map(
     (row, idx) =>
-      services.slice(
+      props.servicesList.slice(
         idx * 2,
         idx * 2 + 2
       ) /*I split the services array in several arrays which have a length equal to two*/
   );
 
+  async function createNewTicket(service_id) {
+    setTicketIdGenerated(await API.generateNewTicket(service_id)); //I store the generated tickedID in a state. It's the value returned from the API call
+    setShow(true);
+  }
   const buttonsservice = serviceRows.map((row, index) => {
     return (
       //Each small array will be mapped in Row and buttons.
       <Row key={index} className="justify-content-between">
         {row.map((service) => {
-          // The  contents of the small arrays are mapped to buttons
+          // The  contents of the small arrays are mapped to buttons. In this way we'll have always two buttons for row.
           return (
             <Col xs={6} className="mt-3">
-              <Button variant="outline-primary" size="lg" className="p-5">
-                {service}
+              <Button
+                onClick={async () => {
+                  createNewTicket(service.service_id); //When the customer clicks the button, It will execute an API that creates the ticket id.
+                }}
+                variant="outline-primary"
+                size="lg"
+                className="p-5 w-75"
+              >
+                {service.service_name}
               </Button>
             </Col>
           );
@@ -27,7 +43,6 @@ function SelectServices(props) {
       </Row>
     );
   });
- 
 
   return (
     <>
@@ -36,6 +51,11 @@ function SelectServices(props) {
           Select your services
         </h1>
         {buttonsservice}
+        <Alert show={show} className="mt-5" variant="primary">
+          <Alert.Heading style={{ textAlign: "center" }}>
+            New Ticket Generated : {ticketIdgenerated}
+          </Alert.Heading>
+        </Alert>
       </Container>
     </>
   );
